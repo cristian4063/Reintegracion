6fddd27c94b8b3f35f73a5b15deb570b39333250
cristian4063 <varela0007@gmail.com>
2014-12-05 09:01:57 -0500

    cambios g¡faqs e index

---
+++ Código App/scripts/bd-copia.js
@@ -0,0 +1,17 @@
+function configurar_db() {
+    function execute(tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS usuario (id, titulo, nombre_tipo, descripcion, vacantes, cargo, nombre_salario, sector, nombre_experiencia, nombre_nivel, profesion, nombre_departamento, nombre_municipio, fecha_publicacion, fecha_vencimiento, dias_vence, empleador, telefono, indicativo, celular, direccion, email, fecha_actualizacion)');
+    }
+
+    function error(error) {
+        console.log("Error al configurar base de datos", error)
+    }
+
+    function exito() {
+        console.log("Configuración exitosa")
+    }
+
+    var db = window.openDatabase("bd_vacantes", "1.0", "Vacantes", 200000);
+    db.transaction(execute, error, exito);
+
+}
